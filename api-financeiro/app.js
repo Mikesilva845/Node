7fc16be945aec5be8d702a4e0fafb2d18c9ef76c
MAYCON NILTON DE JESUS DA SILVA <maycon.njsilva@senacsp.edu.br>
2025-12-10 22:13:20 -0300

    NodeJs

--- api-financeiro/app.js
+++ api-financeiro/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require("cors")
 const financeiroRoutes = require("./routes/financeiroRoutes")
 const authRoutes = require("./routes/authRoutes")
 const categoriaRoutes = require("./routes/categoriaRoutes")
@@ -7,7 +8,8 @@ const PORT = 3000
 const { conexaoBanco } = require("./models")
 
 const app = express()
-app.use(express.json())     
+app.use(cors())
+app.use(express.json())    
 
 // Chama o roteador apropriado à rota
 app.use("/financeiro", financeiroRoutes)
